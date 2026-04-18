132b8fc7cb244aad47c4b3520fccf5d41352172a
Tolosa <jezustolosa1@gmail.com>
2026-04-17 21:01:28 -0700

    sum of n numbers

---
+++ Program2.java
@@ -0,0 +1,12 @@
+public class Program2 {
+    public static void main(String[] args) {
+        int [] arr = {1,76,7756,46,856,4,45};
+        int result = 0;
+        for(int value : arr){
+            result += value;
+        }
+        System.out.println(result);
+
+    }
+    
+}
